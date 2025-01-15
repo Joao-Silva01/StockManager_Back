c62a1329e307a556f832749866466657eaf33e30
João Roberto Silva Felizardo <futurepromise12@gmail.com>
2025-01-15 10:47:41 -0300

    Project Created

---
+++ src/test/java/com/dev/StockManager/StockManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.StockManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
